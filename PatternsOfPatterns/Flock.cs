29ca13947c54141e1b0307bd6b336fc78be02729
Jon Bækken <Jon Bkken>
2016-05-05 22:25:43 +0200

    Added Flocks

--- PatternsOfPatterns/Flock.cs
+++ PatternsOfPatterns/Flock.cs
@@ -1,7 +1,21 @@
+// Composite pattern: treat a collection of objects in the same way as individual objects
 namespace PatternsOfPatterns
 {
     public class Flock : Quackable
     {
+        IList quackers = new List();
         
+        public void Add(Quackable quacker)
+        {
+            quackers.Add(quacker);
+        }
+        
+        public void Quack()
+        {
+            foreach (var quacker in quackers)
+            {
+                quacker.Quack();
+            }        
+        }
     }
 }
